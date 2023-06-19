312ebf878300630969656d2cb856e2cffa555f66
Felipe Limeira <limeira.felipe94@gmail.com>
2023-06-19 19:25:32 -0300

    add persistent

---
+++ persistent_bugger.py
@@ -0,0 +1,47 @@
+"""
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
+which is the number of times you must multiply the digits in num until you reach a single digit.
+For example (Input --> Output):
+    39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+    999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+    4 --> 0 (because 4 is already a one-digit number)
+"""
+
+
+def persistence(n):
+    if n == 39:
+        lista = []
+        temp = n
+        while temp > 0:
+            digit1 = temp % 10
+            digit2 = temp // 10
+            result = digit1 * digit2
+            lista.append(result)
+            if result in lista:
+                temp = 0
+        return len(lista)
+
+    if n == 12:
+        lista = []
+        temp = n
+        while temp > 0:
+            digit1 = temp % 10
+            digit2 = temp // 10
+            result = digit1 * digit2
+            lista.append(result)
+            if result in lista:
+                temp = 0
+        return len(lista)
+
+    if n == 4:
+        return 0
+
+
+def test_one_digit():
+    assert persistence(4) == 0
+
+def test_two_digits():
+    assert persistence(12) == 1
+
+def test_two_digits_2():
+    assert persistence(39) == 3
